273be6477c7818dcaf4ef9caa5fdc74fe52eb76d
Alex Grigore <agworksbox@gmail.com>
2026-03-26 13:18:48 +0000

    first commit from zed on omarchy

--- 01_basic_js/25_arraymap.js
+++ 01_basic_js/25_arraymap.js
@@ -1,15 +1,28 @@
 // Array map() method
 
-let dwarves = ['Bifur', 'Bofur', 'Bombur', 'Fili', 'Kili', 'Oin', 'Gloin', 'Thorin', 'Balin', 'Dwalin', 'Nori', 'Dori'];
+let dwarves = [
+  "Bifur",
+  "Bofur",
+  "Bombur",
+  "Fili",
+  "Kili",
+  "Oin",
+  "Gloin",
+  "Thorin",
+  "Balin",
+  "Dwalin",
+  "Nori",
+  "Dori",
+];
 
 // determine the length of each name and save it in an array
 
-let nameLengths = dwarves.map(function(item, index, array) {
-  return item + ': ' + item.length;
+let nameLengths = dwarves.map(function (item, index, array) {
+  return item + ": " + item.length;
 });
 
 console.log(nameLengths);
-/* 
+/*
 [
   'Bifur: 5',  'Bofur: 5',
   'Bombur: 6', 'Fili: 4',
@@ -19,3 +32,5 @@ console.log(nameLengths);
   'Nori: 4',   'Dori: 4'
 ]
 */
+
+// map() returns a new array containing each modified element
---
+++ 01_basic_js/26_switched_to_zed.js
@@ -0,0 +1,2 @@
+// Swapped to Zed editor on Omarchy
+// Tried NVIM, love it, however I'm not used to vim motions yet and will need to postpone it a little. Will definitely come back to it once I've had the change to give more time to learning vim.
