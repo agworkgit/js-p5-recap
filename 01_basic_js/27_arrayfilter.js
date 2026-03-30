8b764b461f7835da237122887e47c116066491fc
Alex Grigore <agworksbox@gmail.com>
2026-03-30 10:12:09 +0200

    bitwise operator &

--- 01_basic_js/27_arrayfilter.js
+++ 01_basic_js/27_arrayfilter.js
@@ -15,14 +15,19 @@ let oddNums = numbers.filter(function (item, index, array) {
   return item % 2 === 1; // has remainder after division by 2 = odd number
 });
 
+let oddNumsBitwise = numbers.filter((item, index, array) => {
+  return item & 1; // will compare each decimal in binary form to see if it ends in 0 or 1 (if it ends in 1, it is odd and will return true)
+});
+
 let log = console.log;
 
 log("\nSmall numbers:", smallNums);
 log("\nLarge numbers:", bigNums);
 log("\nOdd numbers:", oddNums);
+log("\nOdd numbers bitwise:", oddNumsBitwise);
 
 /* Logs:
 Small numbers: [ 23, 16 ]
 Large numbers: [ 45, 66, 456, 56, 77 ]
-Odd numbers: [ 23, 45, 77 ] 
+Odd numbers: [ 23, 45, 77 ]
 */
