cb4cf6bd2da66109e935653bf3c0ef3cc00526d9
Alex Grigore <agworksbox@gmail.com>
2026-04-24 16:57:39 +0100

    html node equality in js

--- 01_basic_js/54_eq_and_dbl_eq.js
+++ 01_basic_js/54_eq_and_dbl_eq.js
@@ -4,7 +4,7 @@
 // The negative versions are != and !==
 // not equal and strictly non-equal
 
-let a, b, obj1, obj2, copy1, copy2, html1, html2;
+let a, b, obj1, obj2, copy1, copy2;
 a = 5;
 b = 5;
 
@@ -12,7 +12,49 @@ obj1 = { prop: "value" };
 obj2 = { prop: "value" };
 
 copy1 = obj1;
-copy2 = obj2;
+copy2 = obj1;
 
-html1 = document.querySelector("h1");
-html2 = document.querySelector("h1");
+// numbers
+
+if (a == b) {
+  console.log("a b are double equal");
+} else {
+  console.log("a b are not equal");
+} // a b are double eq
+
+if (a === b) {
+  console.log("a b are triple equal");
+} else {
+  console.log("obj1 obj2 are not triple equal");
+} // a b are triple eq
+
+// objects
+
+if (obj1 == obj2) {
+  console.log("obj1 obj2 are double equal");
+} else {
+  console.log("obj1 obj2 are not equal");
+} // obj1 obj2 are NOT double eq
+
+if (obj1 === obj2) {
+  console.log("obj1 obj2 are triple equal");
+} else {
+  console.log("obj1 obj2 are not triple equal");
+} // obj1 obj2 are NOT triple eq
+
+// copies of the same thing
+
+if (copy1 == copy2) {
+  console.log("copy1 copy2 are double equal");
+} else {
+  console.log("copy1 copy2 are not equal");
+} // copy1 copy2 are double eq
+
+if (copy1 === copy2) {
+  console.log("copy1 copy2 are triple equal");
+} else {
+  console.log("copy1 copy2 are not triple equal");
+} // copy1 copy2 are triple eq
+
+// What about if we were to compare HTML DOM nodes?
+// If you compare two nodes asigned to different vars, they'll both be equal in javascript's eyes
