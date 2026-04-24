5ae26ba1c40850daeedda4aca98b0b0330b3114d
Alex Grigore <agworksbox@gmail.com>
2026-04-24 19:58:24 +0100

    void function

---
+++ 01_basic_js/61_void_operator.js
@@ -0,0 +1,12 @@
+// Using the 'void' (expression) operator
+
+void console.log("sample void"); // works like a function but does not return anything
+// evaluates what's between parentheses but returns nothing
+
+function x() {
+  // no return, still returns 'undefined'
+  // a functions must always return something...
+  let a = 1;
+}
+
+let abc = x(); // undefined
