1f4df1cab125004ad1c03d3a4ad5c7636e0f1ed6
Alex Grigore <agworksbox@gmail.com>
2026-04-28 18:15:48 +0100

    for loop

---
+++ 02_eloquent_js/22_for_loops.js
@@ -0,0 +1,30 @@
+/* 
+Notes:
+- Many loops follow the pattern shown in the 'while' examples. First a 'counter' binding is created to track progress of the loop. Then comes a 'while' loop, usually with a test expression that checks for the 'counter' value. At the end of the loop body the 'counter' is updated to track progress.
+*/
+
+let counter = 1;
+let product = 1;
+while (counter <= 5) {
+  product *= counter;
+  counter += 1;
+}
+console.log(product);
+
+/* 
+Notes:
+- Because this pattern is so common, JavaScript and similar languages provide a shorter and more comprehensive form, the 'for loop':
+*/
+
+for (let number = 0; number <= 12; number = number + 2) {
+  console.log(number);
+}
+
+// 0
+// 2
+// ... etc
+
+/* 
+Notes:
+- The only change is that all the statements that are related to the 'state' of the loop are now grouped together after 'for'.
+*/
