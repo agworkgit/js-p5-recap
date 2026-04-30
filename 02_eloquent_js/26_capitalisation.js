2da67df0a2ca6aab88345b2e7fd058a071bda0d9
Alex Grigore <agworksbox@gmail.com>
2026-04-30 06:47:58 +0100

    comments/conventions

---
+++ 02_eloquent_js/26_capitalisation.js
@@ -0,0 +1,14 @@
+/* 
+Notes:
+- Binding names may not contain spaces, yet it is often helpful to see what multiple words are trying to describe.
+- These conventions have been adopted to make that easier:
+*/
+
+// SNAKE CASE
+let fuzzy_little_turtle;
+
+// PASCAL CASE
+let FuzzyLittleTurtle;
+
+// CAMEL CASE (JS Default)
+let fuzzyLittleTurtle;
---
+++ 02_eloquent_js/27_comments.js
@@ -0,0 +1,14 @@
+// Notes:
+// - Often, raw code does not convey all the information you want a program to convey to human readers, or it conveys it in such a cryptic way that people might now undersatand it.
+// - At other times, you might just want to include some related thoughts as part of your program.
+// - A comment is a piece of text that is part of the program but is completely ignored from execution.
+// - JS has two ways of writing comments, to write single-line comments you can use two slash characters (//) and then the comment text after it. Or multi-line comments, which are surrounded by (/* comments */).
+
+let accountBalance = calculateBalance(account);
+// It's a green hollow where a river sings
+accountBalance.adjust();
+// Madly catching white tatters in the grass
+let report = new Report();
+// Where the sun on the proud mountain rings:
+addToReport(accountBalance, report);
+// It's a little valley, foaming like light in a glass
