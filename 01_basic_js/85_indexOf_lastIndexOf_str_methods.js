fb145a6d511a31a9b5715f114f2215fc9ecabf19
Alex Grigore <agworksbox@gmail.com>
2026-05-13 16:33:52 +0100

    pushed from mac: jsfts - indexOf

--- 01_basic_js/85_indexOf_lastIndexOf_str_methods.js
+++ 01_basic_js/85_indexOf_lastIndexOf_str_methods.js
@@ -3,7 +3,8 @@ indexOf() and lastIndexOf() string methods
 Notes:
 - indexOf lets us search for the index of any string inside another string.
 - The searches are case sensitive.
-- If the target index is not found the return is -1
+- If the target index is not found the return is -1.
+- Second argument tells indexOf to start its search from that index onward.
 */
 
 let log = console.log;
@@ -16,3 +17,7 @@ log(sentence.indexOf("end")); // index 7
 log(sentence.lastIndexOf("I")); // index 0
 
 log(sentence.toLowerCase().indexOf("i")); // index 0
+
+log(sentence.indexOf("e", 10));
+// '10' is an 'optional start index' argument
+// index 13
