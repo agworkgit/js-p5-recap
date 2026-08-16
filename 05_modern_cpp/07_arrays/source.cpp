27c34ec407feb8b88345dbc5ad431a3e9b441152
Alex Grigore <agworksbox@gmail.com>
2026-08-16 11:30:39 +0100

    oma: modern cpp - arrays

--- 05_modern_cpp/07_arrays/source.cpp
+++ 05_modern_cpp/07_arrays/source.cpp
@@ -10,5 +10,15 @@ ARRAYS
 
 int main()
 {
+    // This example declares an array of 5 chars.
     char arr[5];
+
+    // To declare an array of type int which holds 5 elements we should use
+    int arr[5];
+
+    // To initialise an array, we can use the initilisiation list {}:
+    int arr[5] = { 10,20,30,40,50 };
+
+    // We can access individual array elements through a subscript [] operator and an index.
+    // The first array element has an index of 0
 }
