e8b6c39740c345c5cdccae8f14f76b4ec2299b6d
Casian Alexandru Grigore <agworksbox@gmail.com>
2026-08-20 17:22:28 +0100

    pcc - recursion exp + int division

--- 06_python_crash/python_work/chapter_3/07_recursive_exponent.py
+++ 06_python_crash/python_work/chapter_3/07_recursive_exponent.py
@@ -22,3 +22,28 @@ print(rec_fun(5)); # 32
 # rec_fun(3) = 4 * 2
 # rec_fun(4) = 8 * 2
 # rec_fun(5) = 16 * 2 = 32
+
+# This function calculates and returns a^n
+
+def rec_g(a, n):
+	if n == 1:
+		return a
+	elif n == 0:
+		return 1
+	else:
+		# Python will do float point division with /
+		# Use // for integer division!
+		return rec_g(a,(n//2)) * rec_g(a,((n+1)//2))
+
+print(rec_g(2,3)) # should give 8
+
+#              rec_g(2, 3)
+#            /             \
+#           /               \
+#  rec_g(2, 1)    *     rec_g(2, 2)
+#       |                /          \
+#       |               /            \
+#   return 2      rec_g(2, 1)  *  rec_g(2, 1)
+#                       |              |
+#                   return 2       return 2
+
