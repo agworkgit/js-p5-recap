c56360ff8ceefbe232333f8b6912f05c60eb1acc
Alex Grigore <agworksbox@gmail.com>
2026-08-21 18:41:55 +0100

    mac: pcc - algos

--- 06_python_crash/python_work/chapter_3/08_partition.py
+++ 06_python_crash/python_work/chapter_3/08_partition.py
@@ -3,11 +3,11 @@
 # It selects the first element as the **pivot**.
 # Then, we should **reorder the list** so that elements **smaller** than the pivot are placed **before the pivot** and elements with **greater values** than the pivot are placed **after the pivot**.
 
-unordered_list = [8, 12, 6, 9, 4, 10, 3, 11]
+unordered_list = [14, 3, 16, 7, 28, 13, 1, 9, 27, 21]
 
 print(unordered_list)
 
-# Practice implementation
+# Practice implementation (naive solution)
 
 def practice_algo(list):
     pivot = list[0]
@@ -26,6 +26,7 @@ def practice_algo(list):
 # print(practice_algo(unordered_list))
 
 # The correct implementation (in-place with i and j)
+
 # Beware that starting index is 0, in the pseudocode it's 1-indexed
 
 def partition_algo(list):
@@ -34,25 +35,33 @@ def partition_algo(list):
     j = len(list) - 1
 
     while i < j:
-        # while list[i](12) <= list[0](8) && i(1) < j(7)
-        while list[i] <= pivot:
-            # i is now 2
+        while i <= j and list[i] <= pivot:
             i += 1
-        # while list[j](3) <= list[0](8) && i(1) < j(7)
-        while list[j] >= pivot:
-            # j is now 6
+        while i <= j and list[j] >= pivot:
             j -= 1
         if i < j:
-        # i(2) < j(6)
-            # swap i with j and j with i
-            # i(6) and j(2)
             list[i], list[j] = list[j], list[i]
-    # end of loop swap list[0] with list[j] and vice versa
     list[0], list[j] = list[j], list[0]
-    # the current pivot position
+
     return j
 
-pivot_position = partition_algo(unordered_list)
+# pivot_position = partition_algo(unordered_list)
 
-print(unordered_list)
-print(pivot_position)
+# print(unordered_list)
+# print(pivot_position)
+
+# Quick sort
+
+def quick_sort(list):
+
+    if len(list) <= 1:
+        return list
+    j = partition_algo(list)
+    left = list[:j]
+    pivot = list[j]
+    right = list[j + 1:]
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+sorted_list = quick_sort(unordered_list)
+print(sorted_list)
